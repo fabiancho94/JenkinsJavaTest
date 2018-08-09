8a88de6c5a4fd37d1b632e226d7dc6ee83fd68e1
Fabian Velez <analista.devops.safesociety@gmail.com>
2018-08-09 17:00:01 -0500

    Example 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+
         JOptionPane.showMessageDialog(null, "Bienvenido de nuevo a JAVA");
+        System.out.println("Hola jenkins");
     }
 
 }
